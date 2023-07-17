e2e8b544b6d5c20b170c0bc35950c73853a4bdbd
Jayakrishna Gandhamalla <gandhamallajayakrishna@hotmail.com>
2023-07-17 05:46:45 +0530

    Made unoptimized images to true to load as is as static pages doesn't support optimisation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   target: "serverless",
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
