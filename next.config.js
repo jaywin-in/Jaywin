5db337222d4456158f9547429d291411437153fb
Jayakrishna Goud Gandhamalla <gandhamallajayakrishna@hotmail.com>
2024-01-09 08:52:36 -0500

    Fixed Static Export Issue

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
