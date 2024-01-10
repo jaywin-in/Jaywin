73cd2e1292b45f25e27a08f658f74e6c0729931a
praveen7777 <48596005+17H51A05K9@users.noreply.github.com>
2024-01-10 19:36:34 +0530

    added images

--- app/certificates/page.js
+++ app/certificates/page.js
@@ -11,58 +11,58 @@ import fssai from "../../public/img/FSSAI.jpg";
 import spices from "../../public/img/spices.jpg";
 import udyam from "../../public/img/udyam.jpg";
 import apeda from "../../public/img/apeda.jpg";
+import { images } from "../../components/certificates";
 
 export default function Home() {
-    return (
-      <>
-        <Head>
-          <title>Benefits</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Navbar />
-        
-        <Container className="w-100 mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-          <Benefit title="Management & Quality" icon={jaywin}>
-          </Benefit>
-          <Benefit title="GST" icon={gst}>
-          </Benefit>
-          <Benefit title="FSSAI" icon={fssai}>
-          </Benefit>
-          <Benefit title="APEDA" icon={apeda}>
-          </Benefit>
-          <Benefit title="UDYAM" icon={udyam}>
-          </Benefit>
-          <Benefit title="Spices" icon={spices}>
-          </Benefit>
-        </Container>
-  
-        <Footer />
-        <PopupWidget />
-      </>
-    );
-  }
-  
-  const Benefit = (props) => {
-    return (
-      <div className="w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-        <Image
-          src={props.icon} className="hover:scale-125 transition duration-500 cursor-pointer"
-          alt="Benefits"
-          placeholder="blur"
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
-        {/* <div className="p-5 items-stretch">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold italic tracking-tight text-gray-900 dark:text-white">
-              {props.title}
-            </h5>
-          </a>
-          <p className="mb-3 font-normal italic text-gray-700 dark:text-gray-400">
-            {props.children}
-          </p>
-        </div> */}
+  return (
+    <>
+      <Head>
+        <title>Benefits</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+
+      <Container className="w-100 mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        <Benefit title="International Organization for Standardization" icon={jaywin}>
+        </Benefit>
+        <Benefit title="Good's Service Tax" icon={gst}>
+        </Benefit>
+        <Benefit title="FSSAI Food Safety and Standards Authority of India" icon={fssai}>
+        </Benefit>
+        <Benefit title="UDYAM Registration Certificate" icon={udyam}>
+        </Benefit>
+        <Benefit title="Agricultural and Processed Food Products Export Development Authority" icon={apeda}>
+        </Benefit>
+        <Benefit title="Certificate of Registration  As Exported of Spices" icon={spices}>
+        </Benefit>
+      </Container>
+      <Footer />
+      <PopupWidget />
+    </>
+  );
+}
+
+const Benefit = (props) => {
+  return (
+    <div className="w-72 shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+      <Image
+        src={props.icon} className="hover:scale-125 transition duration-500 cursor-pointer"
+        alt="Benefits"
+        placeholder="blur"
+        style={{
+          maxWidth: "100%",
+          height: "auto"
+        }} />
+      <div className="p-5 items-stretch">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-light italic tracking-tight text-gray-900 dark:text-white">
+            {props.title}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal italic text-gray-700 dark:text-gray-400">
+          {props.children}
+        </p>
       </div>
-    );
-  };
+    </div>
+  );
+};
